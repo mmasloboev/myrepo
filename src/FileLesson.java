2a20b458842fee2369eb634284af78404bba7286
unknown <masloboevmatvei@ya.ru>
2022-06-19 20:03:56 +0300

    now i knoe files!

--- src/FileLesson.java
+++ src/FileLesson.java
@@ -1,5 +1,18 @@
-public class FileLesson {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class FileLesson {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File(scanner.nextLine());
+        Scanner fileScanner = new Scanner(file);
+
+        while (fileScanner.hasNextLine()) {
+            System.out.println(fileScanner.nextLine());
+        }
+
+        scanner.close();
+        fileScanner.close();
     }
 }
